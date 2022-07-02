2b8da24767de7b3b1379b79e6544738f7afd9dfc
brunbs <babosa227@gmail.com>
2022-07-02 09:23:48 -0300

    First Commit

---
+++ src/main/java/br/com/backendic/BackendIcApplication.java
@@ -0,0 +1,13 @@
+package br.com.backendic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendIcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backendic/BackendIcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backendic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
